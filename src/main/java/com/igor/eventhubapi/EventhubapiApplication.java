84e321df30568809c9c0172962510f9509718bcc
igorj <igor.castro_ext@light.com.br>
2026-02-17 11:05:06 -0300

    first commit

---
+++ src/main/java/com/igor/eventhubapi/EventhubapiApplication.java
@@ -0,0 +1,13 @@
+package com.igor.eventhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhubapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igor/eventhubapi/EventhubapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.eventhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhubapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
